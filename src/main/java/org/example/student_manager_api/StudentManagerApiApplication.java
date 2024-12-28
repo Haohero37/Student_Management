bc781c232579f4db51acd4dfed9007d46f1bb68a
Haohero37 <haotbph32696@fpt.edu.vn>
2024-12-28 14:39:26 +0700

    Build Base Student Manage Project

---
+++ src/main/java/org/example/student_manager_api/StudentManagerApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.student_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagerApiApplication.class, args);
+        System.out.println("Hello HaoDevJava 37!");
+    }
+}
